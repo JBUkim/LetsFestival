3a3439c5ba4e93f396cbedeb0354fcf7c699ef20
JBUkim <91812103@jmail.ac.kr>
2022-12-11 21:43:26 +0900

    4.1

--- pages/festival/[slug].js
+++ pages/festival/[slug].js
@@ -162,6 +162,7 @@ export default function FestivalScreen() {
               </p>
               <a
                 href={`https://map.kakao.com/link/map/${festival.location},${festival.lat},${festival.lng}`}
+                target="_blank"
                 class="mt-8 inline-flex items-center rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
               >
                 <span class="text-sm font-medium">지도 크게보기</span>
